14b4e1274c000bd1004adc3b87e9e39ea8dc8d69
mariannebarreto <barretoink@icloud.com>
2022-07-19 16:10:05 -0500

    NavBar responsiva con nuevo Logo

--- src/pages/Presentation/sections/Presentation.jsx
+++ src/pages/Presentation/sections/Presentation.jsx
@@ -3,22 +3,15 @@ import { Toolbar } from '@mui/material';
 import { Box } from '@mui/system';
 import NavBar from '../components/NavBar';
 
-const drawerWidth = 280;
-
 function Presentation({ children }) {
 	return (
-		<Box
-			sx={{ display: 'flex' }}
-			className="animate__animated animate__fadeIn animate__faster"
-		>
-			<NavBar drawerWidth={drawerWidth} />
-
+		<NavBar>
 			<Box component="main" sx={{ flexGrow: 1, p: 3 }}>
 				<Toolbar />
 
 				{children}
 			</Box>
-		</Box>
+		</NavBar>
 	);
 }
 
